75c6d0d19393a321042f20d365f89457cc72ab15
Tinks18 <tinutani9@gmail.com>
2022-07-29 00:44:37 +0000

    Add frame variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,8 @@ function animate (){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillStyle = 'red';
     // ctx.fillRect(100, 50, 100, 100);
-    ctx.drawImage(playerImage, 0 * spriteWidth, 4 * spriteHeight, 700, 700, 0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    x++;
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+    ctx.drawImage(playerImage, 0 * spriteWidth, 4 * spriteHeight, 700, 700, 0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);s
     // ctx.drawImage(playerImage, 0, 0);
     requestAnimationFrame(animate);
 };
